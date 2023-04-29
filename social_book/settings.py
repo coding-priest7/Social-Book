f984ca21a81931dc6ead8c22ac9a98e939fe09c7
coding-priest7 <sandeep17goswami@gmail.com>
2023-04-30 01:48:20 +0530

    added allowed hosts

--- social_book/settings.py
+++ social_book/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-dkmi#o^)dm^1h&*!4=w%biys(70j^w)ed%h5^$xb5zyoxjnk!_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'social_book.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 DATABASES['default'] = dj_database_url.config()
